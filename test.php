904a8fde69ec3dd60002694f4e3205b98fae623d
atom <ayakotomozawa@gmail.com>
2023-03-26 13:42:30 +0900

    all

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>test page</title>
+</head>
+<body>
+  <h1>見出し</h1>
+  <p>これはテストページです</p>
+  <p>HTML：本日は2023年 3月 1日です</p>
+  <p>PHP：本日は<?php echo date("Y-m-d H:i:s")?>です</p>
+</body>
+</html>
