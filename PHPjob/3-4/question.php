f73ba935521cd23fa592004f5bc558a97b1abe8f
atom <ayakotomozawa@gmail.com>
2023-03-26 16:14:04 +0900

    all

--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -30,21 +30,21 @@
 	<p>お疲れ様です<?php echo $name; ?>さん</p>
 	<form action="answer.php" method="post">
 		<h2>①ネットワークのポート番号は何番？</h2>
-		<?php foreach ($q1 as $value1) { ?>
-			<input type="radio" name="Ans1" value="<?php echo $value1;?>">
-			<?php echo $value1; ?>
+		<?php foreach ($q1 as $value) { ?>
+			<input type="radio" name="Ans1" value="<?php echo $value;?>">
+			<?php echo $value; ?>
     <?php } ?>
 		<br>
 		<h2>②Webページを作成するための言語は？</h2>
-		<?php foreach ($q2 as $value2) { ?>
-			<input type="radio" name="Ans2" value="<?php echo $value2;?>">
-			<?php echo $value2; ?>
+		<?php foreach ($q2 as $value) { ?>
+			<input type="radio" name="Ans2" value="<?php echo $value;?>">
+			<?php echo $value; ?>
     <?php } ?>
 		<br>
 		<h2>③MySQLで情報を取得するためのコマンドは？</h2>
-		<?php foreach ($q3 as $value3) { ?>
-			<input type="radio" name="Ans3" value="<?php echo $value3;?>">
-			<?php echo $value3; ?>
+		<?php foreach ($q3 as $value) { ?>
+			<input type="radio" name="Ans3" value="<?php echo $value;?>">
+			<?php echo $value; ?>
     <?php } ?>
 		<br>
 
--- PHPjob/test/result.php
+++ PHPjob/test/result.php
@@ -1,1 +1,17 @@
 <?php
+$num = $_POST['numbers'];
+$num_arr = str_split($num);
+$num_key = array_rand($num_arr);
+$result = $num_arr[$num_key];
+
+if ($result == 0){
+  $msg = "凶";
+} elseif ($result >=1 && $result <=3) {
+  $msg = "小吉";
+} else {
+  $msg = "";
+}
+
+echo date("Y/m/d",time())."の運勢は<br>
+選ばれた数字は $result <br>
+$msg";
